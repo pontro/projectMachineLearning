9037191918b16fbfbecb6be470fe4087d29bdece
pontro <azendalito@gmail.com>
2024-01-25 17:22:00 -0600

    html listo

--- projectWorlds2024/main.py
+++ projectWorlds2024/main.py
@@ -2,4 +2,18 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url_lec = "https://gol.gg/tournament/tournament-stats/LCK%20Spring%202024/"
+url_lck = "https://gol.gg/tournament/tournament-stats/LCK%20Spring%202024/"
+
+def getHtml(url):
+    page = requests.get(url)
+    html_page = BeautifulSoup(page.content, "html.parser")
+    return html_page
+
+
+
+def main():
+    html_page = getHtml(url_lck)
+    print(html_page)
+
+
+main()
