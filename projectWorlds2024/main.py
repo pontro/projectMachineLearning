5d919bb544ebe3023244716cc11dd9808611f2ea
pontro <azendalito@gmail.com>
2024-01-25 19:40:09 -0600

    asdagar

--- projectWorlds2024/main.py
+++ projectWorlds2024/main.py
@@ -7,7 +7,7 @@ url_lck = "https://gol.gg/tournament/tournament-ranking/LCK%20Spring%202024/"
 def getHtml(url):
     page = requests.get(url)
     html_page = BeautifulSoup(page.content, "html.parser")
-    
+
     return html_page
 
 def getTeamNames(html_page):
@@ -18,9 +18,16 @@ def getTeamNames(html_page):
 
     return teams
 
+def getTeamWinRate(html_page):
+    winRate=html_page.find_all('div', class_= "col-auto pl-1 position-absolute")
+
+    for element in winRate:
+        print(element.string)
+
 def main():
     html_page = getHtml(url_lck)
     getTeamNames(html_page)
+    getTeamWinRate(html_page)
    
 
 
