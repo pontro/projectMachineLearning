71b84b0ca75ba7e3bb24d8a88edebee4c73cc5c6
pontro <azendalito@gmail.com>
2024-01-25 19:12:32 -0600

    teamNamesDone

--- projectWorlds2024/main.py
+++ projectWorlds2024/main.py
@@ -7,11 +7,16 @@ url_lck = "https://gol.gg/tournament/tournament-ranking/LCK%20Spring%202024/"
 def getHtml(url):
     page = requests.get(url)
     html_page = BeautifulSoup(page.content, "html.parser")
+    
     return html_page
 
 def getTeamNames(html_page):
-    nameSTR = re.compile(r'Hanwha Life eSports stats in LCK SPRING 2024')
-    print (html_page.find_all('td', class_= "footable-visible footable-first-column"))
+    teams = html_page.find_all('a', title = re.compile("stats in LCK"))
+
+    for element in teams:
+        print(element.string)
+
+    return teams
 
 def main():
     html_page = getHtml(url_lck)
