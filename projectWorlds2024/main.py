9b7c7b71fdf56af8f6bd87af54adf242892296c6
pontro <azendalito@gmail.com>
2024-01-27 17:10:36 -0600

    dataset ya casi llea

--- projectWorlds2024/main.py
+++ projectWorlds2024/main.py
@@ -1,8 +1,11 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+
+import pandas as pd
  
-url_lck = "https://gol.gg/tournament/tournament-ranking/LCK%20Spring%202024/"
+url_lck = "https://gol.gg/tournament/tournament-ranking/LCK%20Spring%202023/"
 
 def getHtml(url):
     page = requests.get(url)
@@ -21,16 +24,38 @@ def getTeamWinRate(html_page):
     wr = []
     for element in winRate:
         wr.append(element.string)
+    for i in range(len(wr)):
+        wr[i] = int (wr[i].strip("%"))
     return wr
 
-def joinWinRateName(arr1, arr2, html_page):
+def getTeamGDM(html_page):
+    team_rows = html_page.find_all('tr')[1:]
+    teams_stats = []
+    for row in team_rows:
+        data_cells = row.find_all('td', class_='text-cenoter')
+        for data_cell in data_cells:
+            stats = data_cell.text.strip()
+            teams_stats.append(stats)
 
-   print(2)
+    teams_GDM = []
+    counter = 0
+    for team_stat in teams_stats:
+        counter += 1
+        if counter % 5 == 0:
+            teams_GDM.append(team_stat)
+            counter = 0
+
+
+    for i in range(len(teams_GDM)):
+        teams_GDM[i] = int (teams_GDM[i])
+    return teams_GDM
     
 
 def main():
     html_page = getHtml(url_lck)
-    joinWinRateName(getTeamNames(html_page), getTeamWinRate(html_page), html_page)
-
-
+    data = {'Team': getTeamNames(html_page), 'WinRate': getTeamWinRate(html_page)}
+    dataset = pd.DataFrame(data)
+    x = dataset.iloc[:, -2:]
+    print(x)
+    
 main()
