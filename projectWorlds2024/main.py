fffcdb9fb383aef5dcb4edf999e0397848b47ae3
pontro <azendalito@gmail.com>
2024-01-28 02:35:29 -0600

    model finished

--- projectWorlds2024/main.py
+++ projectWorlds2024/main.py
@@ -1,14 +1,27 @@
 import requests
 import re
-from bs4 import BeautifulSoup
-
 import pandas as pd
+import numpy as np 
+from bs4 import BeautifulSoup
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC 
 
 url_lck = "https://gol.gg/tournament/tournament-ranking/LCK%20Spring%202024/"
+#2023 urls
 lck2023url = "https://gol.gg/tournament/tournament-ranking/LCK%20Summer%20Playoffs%202023/"
+lck23results = "https://gol.gg/tournament/tournament-matchlist/LCK%20Summer%20Playoffs%202023/"
+
+#2022 urls
 lck2022url = "https://gol.gg/tournament/tournament-ranking/LCK%20Summer%20Playoffs%202022/"
-lck2021url = "https://gol.gg/tournament/tournament-ranking/LCK%20Summer%20Playoffs%202021/"
 lck22results = "https://gol.gg/tournament/tournament-matchlist/LCK%20Summer%20Playoffs%202022/"
+
+#2021 urls
+lck2021url = "https://gol.gg/tournament/tournament-ranking/LCK%20Summer%20Playoffs%202021/"
+lck21results = "https://gol.gg/tournament/tournament-matchlist/LCK%20Summer%20Playoffs%202021/"
+
+#2020 urls
+lck2020url = "https://gol.gg/tournament/tournament-ranking/LCK%20Summer%20Playoffs%202020/"
+lck20results = "https://gol.gg/tournament/tournament-matchlist/LCK%20Summer%20Playoffs%202020/"
 
 def getHtml(url):
     page = requests.get(url)
@@ -74,27 +87,65 @@ def getWinners(html_page, lck2022):
     return wins
     
 def main():
-    html_page = getHtml(url_lck)
-    getTeamNames(html_page)
 
+    # training data
+    # 2020 
+    lck2020 = getHtml(lck2020url)
+    lckResults20 = getHtml(lck20results)
+    dataLCK20 = {'Team': getTeamNames(lck2020), 'WinRate': getTeamWinRate(lck2020), 'GDM': getTeamGDM(lck2020), 'Wins': getWinners(lckResults20, lck2020)}
+    dfLCK20 = pd.DataFrame(dataLCK20)
+
+    # 2021 
+    lck2021 = getHtml(lck2021url)
+    lckResults21 = getHtml(lck21results)
+    dataLCK21 = {'Team': getTeamNames(lck2021), 'WinRate': getTeamWinRate(lck2021), 'GDM': getTeamGDM(lck2021), 'Wins': getWinners(lckResults21, lck2021)}
+    dfLCK21 = pd.DataFrame(dataLCK21)
+
+    # 2022 
     lck2022 = getHtml(lck2022url)
     lckResults22 = getHtml(lck22results)
-
     dataLCK22 = {'Team': getTeamNames(lck2022), 'WinRate': getTeamWinRate(lck2022), 'GDM': getTeamGDM(lck2022), 'Wins': getWinners(lckResults22, lck2022)}
     dfLCK22 = pd.DataFrame(dataLCK22)
     
+    #concat dataFrames
+    trainingDataFrame = pd.concat([dfLCK22, dfLCK21, dfLCK20])
 
-    trainingData = dfLCK22.iloc[:, -3:].values
-
-    print(trainingData)
-
+    #x_train and y_train def
+    x_train = trainingDataFrame.iloc[:, -3:-1].values
+    y_train = trainingDataFrame.iloc[:, -1:].values
+    y_train = y_train.ravel()
 
     #test data
+    #2023
     lck2023 = getHtml(lck2023url)
-    dataLCK23 = {'Team': getTeamNames(lck2023), 'WinRate': getTeamWinRate(lck2023), 'GDM': getTeamGDM(lck2023)}
+    lckResults23 = getHtml(lck23results)
+    dataLCK23 = {'Team': getTeamNames(lck2023), 'WinRate': getTeamWinRate(lck2023), 'GDM': getTeamGDM(lck2023), 'Wins': getWinners(lckResults23, lck2023)}
     dfLCK23 = pd.DataFrame(dataLCK23)
+    testingDataFrame = dfLCK23
 
-    testData = dfLCK23.iloc[:, -2:].values
-    teamNames = pd.concat([dfLCK22, dfLCK23])
+    x_test = testingDataFrame.iloc[:, -3:-1].values
+    y_test = testingDataFrame.iloc[:, -1:].values
+
+    x_test_original = x_test.copy()
+
+    #standarize xtrain and xtest so we can use them for inputs for training or evaluating
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(x_test)
+
+    classifier = SVC(kernel = 'linear', random_state=0)
+    classifier.fit(x_train, y_train)
+
+    y_pred = classifier.predict(x_test)
+    predictions = y_pred.reshape(len(y_pred),1)
+
+    tests = y_test.reshape(len(y_test), 1)
+
+    print(predictions)
+
+    #results = {'Team': getTeamNames(lck2023), 'WinRate': getTeamWinRate(lck2023), 'GDM': getTeamGDM(lck2023), 'Wins': getWinners(lckResults23, lck2023), 'Wins Pred': predictions}
+    #results = pd.DataFrame(results)
+
+
 
 main()
